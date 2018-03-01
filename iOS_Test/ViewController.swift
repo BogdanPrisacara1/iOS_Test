df4dae9b9e49be0edf45cfa4f5ede752bea972c1
Bogdan Prisacara <bogdan.prise@gmail.com>
2018-03-01 16:49:01 +0200

    Initial Commit

---
+++ iOS_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_Test
+//
+//  Created by Zumzet Mobile on 01/03/2018.
+//  Copyright © 2018 Bogdan Prisacara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
